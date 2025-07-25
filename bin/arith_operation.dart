256382325ee8d77385488357142334b2cb4b2ab8
Shakirullah <omotososakiru25@gmail.com>
2025-07-25 01:29:20 +0100

    new method: added multiplication method

--- bin/arith_operation.dart
+++ bin/arith_operation.dart
@@ -2,14 +2,16 @@ void main() {
   ArithOperations operations = ArithOperations(5, 6);
   var add = operations.add("Addition");
   var subtract = operations.subtract("Subtraction");
+  var multiply = operations.multiply("Multiplication", 10, 5);
   print(add);
   print(subtract);
+  print(multiply);
 }
 
 class ArithOperations {
   double x;
   double y;
-  
+
   ArithOperations(this.x, this.y);
 
   String add(String operationType) {
@@ -19,5 +21,8 @@ class ArithOperations {
   String subtract(String operationType) {
     return "$operationType: ${x - y}";
   }
-  
+
+  String multiply(String operationType, double a, double b) {
+    return "$operationType: ${a * b}";
+  }
 }
