dcd81098d94b48944809f95b7ab03378c44406cc
Shakirullah <omotososakiru25@gmail.com>
2025-07-25 01:19:39 +0100

    stop tracking .history/ dir

--- .history/bin/arith_operation_20250725002127.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:arith_operation/arith_operation.dart' as arith_operation;
-
-// void main(List<String> arguments) {
-//   print('Hello world: ${arith_operation.calculate()}!');
-// }
